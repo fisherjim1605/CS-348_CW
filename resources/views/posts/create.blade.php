0cb6af8b57e299138b8cabb97c6db099c92f8245
fisherjim1605 <fisherjim1605@gmail.com>
2021-01-03 17:07:27 +0000

    added validation message for post creation, added card views to post creation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,17 +3,36 @@
 @section('title', 'Not a Facebook Rip-off')
 
 @section('content')
-    <p>Create New Post</p>
-    
-    <form method="POST" action="{{ route('posts.store') }}">
-        @csrf
-        <p>Title: <input type="text" name="title"
-            value="{{ old('title') }}"></p>
-        <p>Image URL (Optional): <input type="text" name="image_url"
-            value="{{ old('image_url') }}"></p>
-        <p>Content: <input type="text" name="content"
-            value="{{ old('content') }}"></p>
-        <input type="submit" value="Create Post">
-        <a href="{{ route('posts.index') }}">Cancel</a>
-    </form>        
+    @if($errors->any())        
+            <div class="container">
+                <div class="card">     
+                    <div class="card-header">System Message</div>
+                    <div class="card-body"> 
+                        @foreach($errors->all() as $error)
+                            <p>{{ $error }}</p>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            <br>
+    @endif
+    <div class="container">
+        <div class="card">        
+            <div class="card-header">Create New Post</div>
+                <div class="card-body">    
+                    <form method="POST" action="{{ route('posts.store') }}">
+                        @csrf
+                        <p>Title: <input type="text" name="title"
+                            value="{{ old('title') }}"></p>
+                        <p>Image URL (Optional): <input type="text" name="image_url"
+                            value="{{ old('image_url') }}"></p>
+                        <p>Content: <input type="text" name="content"
+                            value="{{ old('content') }}"></p>
+                        <input type="submit" value="Create Post">
+                        <a href="{{ route('posts.index') }}">Cancel</a>
+                    </form>        
+                </div>      
+            </div>      
+        </div>      
+    </div>
 @endsection
